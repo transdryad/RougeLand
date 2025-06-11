ba438f4df8ad01a3deca69c69bf0ca4e84e6634a
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-06-11 14:25:11 -0600

    Better errors when thrown during log init.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include "include/libtcod.hpp"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include <string>
 #include <fmt/core.h>
+#include <cstdio>
 
 int main() {
     std::shared_ptr<spdlog::logger> logger;
@@ -11,7 +11,7 @@ int main() {
         logger = spdlog::basic_logger_mt("file", "log.txt");
         spdlog::set_pattern("[%Y-%m-%d %T.%e] [%l] %v");
     } catch (const spdlog::spdlog_ex &ex) {
-        std::cerr << "Log init failed: " << ex.what() << std::endl;
+        fmt::print(stderr, "Log init failed: {}\n", ex.what());
         return EXIT_FAILURE;
     }
     logger->info("Welcome to RougeLand!");
