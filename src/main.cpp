e1ce5c5031f4bc43388229087f7d1c908c678e9b
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-06-11 18:32:51 -0600

    Begin TCOD, custom tileset.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 #include <string>
 #include <fmt/core.h>
 #include <cstdio>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
+int main(const int argc, char* argv[]) {
     std::shared_ptr<spdlog::logger> logger;
     try {
         logger = spdlog::basic_logger_mt("file", "log.txt");
@@ -15,14 +17,36 @@ int main() {
         return EXIT_FAILURE;
     }
     logger->info("Welcome to RougeLand!");
-    logger->info("Initializing libtcod.");
-    TCODConsole::initRoot(80,50,"RougeLand",false);
-    while (!TCODConsole::isWindowClosed()) {
-	TCOD_key_t key;
-	TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,null);
-	TCODConsole::root->clear();
-	TCODConsole::root->putChar(40,25,'@');
-	TCODConsole::flush();
+    logger->info("Initializing libTCOD.");
+
+    auto tileset = tcod::load_tilesheet("src/tileset.png",{16, 16}, tcod::CHARMAP_CP437);
+
+    auto console = tcod::Console{80, 45};
+    auto params = TCOD_ContextParams{};
+
+    params.console = console.get();
+    params.window_title = "RougeLand";
+    params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
+    params.vsync = true;
+    params.argc = argc;
+    params.argv = argv;
+    params.tileset = tileset.get();
+
+    auto context = tcod::Context(params);
+
+    logger->info("Entering main loop.");
+    while (true) {
+        console.clear();
+        tcod::print(console, {0, 0}, "Testing RougeLand", std::nullopt, std::nullopt);
+        context.present(console);
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            context.convert_event_coordinates(event);
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    return EXIT_SUCCESS;
+            }
+        }
     }
-    return EXIT_SUCCESS;
+
 }
